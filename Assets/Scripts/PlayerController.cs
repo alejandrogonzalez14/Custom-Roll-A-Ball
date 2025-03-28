5c9f2ccc99ca974373d20e0dd711f16c5cc6fa8f
alejandrogonzalez14 <alejandro.gonzalez14@estudiant.upf.edu>
2025-03-28 14:37:20 +0100

    adding AI navigation (complete)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -76,6 +76,19 @@ public class PlayerController : MonoBehaviour
         if (score >= 12)
         {
             winTextObject.SetActive(true);
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            // Destroy the current object
+            Destroy(gameObject);
+            // Update the winText to display "You Lose!"
+            winTextObject.gameObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
         }
     }
 }
